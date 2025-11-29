ec8946a4ceac54226d510517e16bc00a574ecc69
Jonatan Nevo <jonatannevo-git@proton.me>
2025-11-29 15:58:13 +0200

    update ports

--- ports/portal-serialization/portfile.cmake
+++ ports/portal-serialization/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
         OUT_SOURCE_PATH REPO_ROOT_PATH
         REPO JonatanNevo/portal-framework
-        REF af8e1bad340b7689964a3b592cad642c0965c089
-        SHA512 c68f00ec54f7d4aa1e59fc5dde8399e961d6e40a984d2d5adc19dd673b590b2bb08f45cf303f3265499d1aaff5fbd6355c33e0b3da92b01a5aeeaf7d27222df1
+        REF 029d54d221b3d185eb1e7f99139bb76013f1a1f4
+        SHA512 6ed7291d2e5b0fed71937d561e6e3e0f66e05415af6773efa1874dc217022c61ff795b348d35e72d81784019f0c2b1ba2872238b9343c2edcad08edf18352c76
         HEAD_REF fix/vcpkg-packaging
 )
 
